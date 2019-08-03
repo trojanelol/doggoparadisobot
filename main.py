9981435475ea88a69c38768cd579645db86de48e
trojanelol <42861361+trojanelol@users.noreply.github.com>
2019-08-03 13:49:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#import all libraries
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+#access API and get the image url
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def doggo(bot, update):
+    #Get image url
+    url = get_url()
+    #Get the recipient’s ID
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('891362789:AAFZZgY6BhSeRHUJx1jRRiFRrI-WJtCdrqU')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('doggo',doggo))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
